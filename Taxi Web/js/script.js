baa337f2426636bedb74f28a67f749b0a97a4581
vhlinh <vuhoanglinh1997@gmail.com>
2017-11-30 15:07:10 +0700

    Update Website UI

--- Taxi Web/js/script.js
+++ Taxi Web/js/script.js
@@ -23,59 +23,31 @@ $(document).ready(function () {
 });
 
 
-
-
-
-//Slide effect
-var sliderImages = document.querySelectorAll(".slide"),
-    arrowLeft = document.querySelector("#arrow-left"),
-    arrowRight = document.querySelector("#arrow-right"),
-    current = 0;
-
-// Clear all images
-function reset() {
-    for (var i = 0; i < sliderImages.length; i++) {
-        sliderImages[i].style.display = "none";
-    }
-}
-
-// Init slider
-function startSlide() {
-    reset();
-    sliderImages[0].style.display = "block";
-}
-
-// Show prev
-function slideLeft() {
-    reset();
-    sliderImages[current - 1].style.display = "block";
-    current--;
-}
-
-// Show next
-function slideRight() {
-    reset();
-    sliderImages[current + 1].style.display = "block";
-    current++;
-}
-
-// Left arrow click
-arrowLeft.addEventListener("click", function () {
-    if (current === 0) {
-        current = sliderImages.length;
-    }
-    slideLeft();
+$(document).ready(function(){
+    // Activate Carousel
+    $("#slide-wrap").carousel();
+    
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#slide-wrap").carousel(0);
+        
+    });
+    $(".item2").click(function(){
+        $("#slide-wrap").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#slide-wrap").carousel(2);
+    });
+   
+    
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#slide-wrap").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#slide-wrap").carousel("next");
+    });
 });
-
-// Right arrow click
-arrowRight.addEventListener("click", function () {
-    if (current === sliderImages.length - 1) {
-        current = -1;
-    }
-    slideRight();
-});
-
-startSlide();
 
 
 $('.flip').hover(function () {
